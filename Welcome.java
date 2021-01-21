ded27dad4476b127c1837c0d349a6c84044b92ab
jega <jega@gmail.com>
2021-01-21 11:13:59 +0530

    First Commit

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Hello
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome 2021!");
+        System.out.println("Have Nice Day!!!")
+    }
+}
